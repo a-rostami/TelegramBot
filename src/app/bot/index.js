2b291f50d698ba7a366342ca21ac35aea680aa63
Arash Rostami <a.rostami2400@gmail.com>
2023-01-01 01:10:54 +0330

    update token name

--- src/app/bot/index.js
+++ src/app/bot/index.js
@@ -1,9 +1,9 @@
 const {Telegraf} = require("telegraf");
 const LocalSession = require('telegraf-session-local')
 const {mainButtons} = require("./utils/ButtonManager")
-const KeyboardMiddleware =require("./middleware/KeyboardMiddleware")
-const ActionMiddleware =require("./middleware/ActionMiddleware")
-const SessionMiddleware =require("./middleware/SessionMiddleware")
+const KeyboardMiddleware = require("./middleware/KeyboardMiddleware")
+const ActionMiddleware = require("./middleware/ActionMiddleware")
+const SessionMiddleware = require("./middleware/SessionMiddleware")
 const {START_MESSAGE} = require("./data/category.json");
 
 let bot;
@@ -11,7 +11,7 @@ let bot;
 async function startBot() {
     bot = new Telegraf(process.env.BOT_TOKEN)
     await bot.launch();
-    bot.use(new LocalSession({database : "session.json"}))
+    bot.use(new LocalSession({database: "session.json"}))
     bot.use(KeyboardMiddleware)
     bot.use(SessionMiddleware)
     bot.use(ActionMiddleware)
